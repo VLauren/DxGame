9be7d7aa575953bd0178686b358dddf98da6f882
VLauren <laurentino.vilchez@gmail.com>
2026-06-23 08:53:59 +0200

    assimp to dx matrix format conversion helper

--- src/Actors/RenderComponents.cpp
+++ src/Actors/RenderComponents.cpp
@@ -23,6 +23,16 @@
 template <typename T>
 using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+inline DirectX::XMMATRIX AssimpToDxMatrix(const aiMatrix4x4& m)
+{
+    return DirectX::XMMATRIX(
+        m.a1, m.b1, m.c1, m.d1,
+        m.a2, m.b2, m.c2, m.d2,
+        m.a3, m.b3, m.c3, m.d3,
+        m.a4, m.b4, m.c4, m.d4
+        );
+}
+
 // =================
 // Render Component
 // =================
